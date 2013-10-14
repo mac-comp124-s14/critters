9937905f4a5fa1e6b1453a6677072c4413835a56
shilad <shilad@gmail.com>
2013-10-13 21:28:29 -0500

    Initial

---
+++ src/edu/macalester/comp124/critters/Eye.java
@@ -0,0 +1,82 @@
+package edu.macalester.comp124.critters;
+
+import acm.graphics.GCompound;
+import acm.graphics.GObject;
+import acm.graphics.GOval;
+import acm.graphics.GPoint;
+
+import java.awt.*;
+
+public class Eye {
+    private final GObject graphics;
+    private final GObject pupil;
+    private final GPoint pupilRestPosition;
+    private final double radius;
+    
+    /**
+     * Creates a white eye with a black pupil, a white ocular highlight, and a colored iris.
+     * @param x             Eye position
+     * @param y             Eye position
+     * @param r             Eye radius
+     * @param pupilSize     Proportion of the pupil radius to the eye radius
+     * @param highlightSize Proportion of the highlight radius to the eye radius
+     * @param iris          Color of the iris
+     * @return
+     */
+    public static Eye createStandardEye(double x, double y, double r, double pupilSize, double highlightSize, Color iris) {
+        GOval white = new GOval(-r, -r, r * 2, r * 2);
+        white.setFilled(true);
+        white.setFillColor(Color.WHITE);
+        
+        GCompound pupil = new GCompound();
+        
+        double pupilR = r * pupilSize;
+        GOval pupilMain = new GOval(-pupilR, -pupilR, pupilR * 2, pupilR * 2);
+        pupilMain.setFilled(true);
+        pupilMain.setFillColor(Color.BLACK);
+        pupilMain.setColor(iris);
+        pupil.add(pupilMain);
+        
+        double highlightR = r * highlightSize;
+        GOval highlight = new GOval(0, -highlightR * 2, highlightR * 2, highlightR * 2);
+        highlight.setColor(new Color(0, 0, 0, 0));
+        highlight.setFilled(true);
+        highlight.setFillColor(new Color(255, 255, 255, 200));
+        pupil.add(highlight);
+        
+        GCompound group = new GCompound();
+        group.add(white);
+        group.add(pupil);
+        group.setLocation(x, y);
+        
+        return new Eye(group, pupil, r - pupilR - 2);
+    }
+    
+    /**
+     * Creates a new eye with custom graphics. The given pupil object with move on the edge of a circle
+     * centered about its current location.
+     */
+    public Eye(GCompound group, GObject pupil, double radius) {
+        this.graphics = group;
+        this.pupil = pupil;
+        pupilRestPosition = pupil.getLocation();
+        this.radius = radius;
+    }
+
+    public GObject getGraphics() {
+        return graphics;
+    }
+    
+    /**
+     * Causes the eyes to move in the direction of the given vector.
+     * The dt parameter is necessary because the eyes do not move instantaneously.
+     */
+    public void lookInDirectionOf(double dx, double dy, double dt) {
+        double dist = Math.hypot(dx, dy);
+        dx /= dist;
+        dy /= dist;
+        pupil.setLocation(
+            CritterUtils.blend(pupil.getX(), pupilRestPosition.getX() + radius * dx, Math.pow(0.05, dt)),
+            CritterUtils.blend(pupil.getY(), pupilRestPosition.getY() + radius * dy, Math.pow(0.05, dt)));
+    }
+}
---
+++ src/edu/macalester/comp124/critters/Leg.java
@@ -0,0 +1,64 @@
+package edu.macalester.comp124.critters;
+
+import acm.graphics.GObject;
+import acm.graphics.GPoint;
+
+public class Leg {
+    private final GObject graphics;
+    private final GPoint restPosition;
+    private double rangeOfMotion;
+    private boolean anchored;
+    
+    /**
+     * Critters should instantiate legs from their createGraphics() methods.
+     * 
+     * @param graphics         The visual representation of the leg, positioned at rest.
+     * @param rangeOfMotion    How far away from its rest position the leg is allowed to move.
+     */
+    public Leg(GObject graphics, double rangeOfMotion) {
+        this.graphics = graphics;
+        this.restPosition = graphics.getLocation();
+        this.rangeOfMotion = rangeOfMotion;
+    }
+
+    public GObject getGraphics() {
+        return graphics;
+    }
+    
+    /**
+     * Called after the body has moved to update the leg motion accordingly.
+     * This method does not use a particularly convincing leg motion algorithm;
+     * low-budgets Saturday morning cartoons would be proud.
+     */
+    public void bodyMovedBy(double dx, double dy) {
+        if(anchored) {
+            graphics.move(-dx, -dy);
+        } else {
+            double speed = Math.hypot(dx, dy),
+                   targetX = restPosition.getX() + dx / speed * rangeOfMotion,
+                   targetY = restPosition.getY() + dy / speed * rangeOfMotion,
+                   toTargetX = targetX - graphics.getX(),
+                   toTargetY = targetY - graphics.getY(),
+                   distToTarget = Math.hypot(toTargetX, toTargetY);
+            graphics.move(
+                toTargetX / distToTarget * speed * 1.5,
+                toTargetY / distToTarget * speed * 1.5);
+        }
+        
+        double distention = Math.hypot(graphics.getX() - restPosition.getX(), graphics.getY() - restPosition.getY());
+        if(distention >= rangeOfMotion)
+            anchored = !anchored;
+    }
+    
+    /**
+     * An anchored leg roughly retains its current absolute position on the screen.
+     * An unanchored leg quickly moves ahead of the current motion.
+     */
+    public boolean isAnchored() {
+        return anchored;
+    }
+
+    public void setAnchored(boolean anchored) {
+        this.anchored = anchored;
+    }
+}
