13a2948a6082452b5abea24114b0689daeb0dd83
lzoeckle <lzoeckle@macalester.edu>
2013-10-15 09:59:29 -0500

    .

---
+++ src/edu/macalester/comp124/critters/Falcor.java
@@ -0,0 +1,15 @@
+package edu.macalester.comp124.critters;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: lzoeckle
+ * Date: 10/15/13
+ * Time: 9:54 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Falcor extends Critter {
+    @Override
+    protected void buildGraphics() {
+
+    }
+}
