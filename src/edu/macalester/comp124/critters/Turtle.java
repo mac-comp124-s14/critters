12a34bd768dda0e5c3694227120b4bea582542bd
mfinegold28 <sfinegol@macalester.edu>
2013-10-15 10:50:21 -0500

    Work done by Andrew and Sam.

--- src/edu/macalester/comp124/critters/Turtle.java
+++ src/edu/macalester/comp124/critters/Turtle.java
@@ -25,36 +25,6 @@ public class Turtle extends Critter {
             leg1.setFillColor(Color.GREEN);
             addLeg(new Leg(leg1, 2));
         }
-//        GRect leg2  = new GRect(-10, 0, 10, 20);
-//        leg2.setColor(Color.GREEN);
-//        leg2.setFilled(true);
-//        leg2.setFillColor(Color.GREEN);
-//        addLeg(new Leg(leg2, 2));
-//
-//        GRect leg3  = new GRect(10, 0, 10, 20);
-//        leg3.setColor(Color.GREEN);
-//        leg3.setFilled(true);
-//        leg3.setFillColor(Color.GREEN);
-//        addLeg(new Leg(leg3, 2));
-//
-//        GRect leg4  = new GRect(30, 0, 10, 20);
-//        leg4.setColor(Color.GREEN);
-//        leg4.setFilled(true);
-//        leg4.setFillColor(Color.GREEN);
-//        addLeg(new Leg(leg4, 2));
-
-
-//        GRect body = new GRect(-20, -30, 40, 40);
-//        body.setColor(strokeColor);
-//        body.setFilled(true);
-//        body.setFillColor(fillColor);
-//        getGraphics().add(body);
-//
-//        GRect head = new GRect(-18, -56, 36, 20);
-//        head.setColor(strokeColor);
-//        head.setFilled(true);
-//        head.setFillColor(fillColor);
-//        getGraphics().add(head);
 
 
         GArc shell = new GArc(80, 50, 0, 180);
@@ -66,6 +36,7 @@ public class Turtle extends Critter {
         head.setFilled(true);
         head.setFillColor(Color.GREEN);
         getGraphics().add(head, 40, -5);
+
 
         Eye leftEye = new Eye(6, 0.26, 0.18, Color.BLUE);
         //Eye rightEye = new Eye(6, 0.26, 0.18, Color.BLUE);
